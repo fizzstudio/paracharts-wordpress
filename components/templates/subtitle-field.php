4fe4c98d6973fb8f43ebec3cc1705dda8a59b6b0
Joe Dolson <design@joedolson.com>
2025-07-09 12:46:20 -0500

    Add wrap on subtitle field

--- components/templates/subtitle-field.php
+++ components/templates/subtitle-field.php
@@ -1,2 +1,4 @@
-<label for="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" class="screen-reader-text">Add Chart Subtitle</label>
-<input class="input" type="text" name="<?php echo esc_attr( $this->get_field_name( 'subtitle' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" value="<?php echo esc_attr( $post_meta['subtitle'] ); ?>" placeholder="<?php esc_attr_e( 'Add subtitle', 'paracharts' ); ?>" style="width: 100%;" />
+<div class="subtitlewrap">
+	<label for="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" class="screen-reader-text">Add Chart Subtitle</label>
+	<input class="input" type="text" name="<?php echo esc_attr( $this->get_field_name( 'subtitle' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" value="<?php echo esc_attr( $post_meta['subtitle'] ); ?>" placeholder="<?php esc_attr_e( 'Add subtitle', 'paracharts' ); ?>" style="width: 100%;" />
+</div>
