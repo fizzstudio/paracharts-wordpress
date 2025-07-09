634de9cfdf25dd349de2a6d05e81b03dd25ee063
Joe Dolson <design@joedolson.com>
2025-07-09 12:43:29 -0500

    Add label to subtitle field

--- components/templates/subtitle-field.php
+++ components/templates/subtitle-field.php
@@ -1,1 +1,2 @@
-<input class="input" type="text" name="<?php echo esc_attr( $this->get_field_name( 'subtitle' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" value="<?php echo esc_attr( $post_meta['subtitle'] ); ?>" placeholder="<?php esc_attr_e( 'Enter subtitle here', 'paracharts' ); ?>" style="width: 100%;" />
+<label for="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" class="screen-reader-text">Add Chart Subtitle</label>
+<input class="input" type="text" name="<?php echo esc_attr( $this->get_field_name( 'subtitle' ) ); ?>" id="<?php echo esc_attr( $this->get_field_id( 'subtitle' ) ); ?>" value="<?php echo esc_attr( $post_meta['subtitle'] ); ?>" placeholder="<?php esc_attr_e( 'Add subtitle', 'paracharts' ); ?>" style="width: 100%;" />
