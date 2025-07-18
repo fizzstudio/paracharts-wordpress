d202839370c9cb0a8c737702e2d3495997aa1823
Joe Dolson <design@joedolson.com>
2025-07-18 10:40:17 -0500

    Remove unneeded canvas element

--- components/templates/chart-meta-box.php
+++ components/templates/chart-meta-box.php
@@ -2,7 +2,6 @@
 // Show what the graph looks like
 echo $chart;
 ?>
-<canvas id="paracharts-canvas-render-<?php echo absint( $post->ID ); ?>" class="hide"></canvas>
 <textarea name="<?php echo esc_attr( $this->get_field_name( 'img' ) ); ?>" rows="8" cols="40" id="<?php echo esc_attr( $this->get_field_id( 'img' ) ); ?>" class="hide"></textarea>
 <?php
 
