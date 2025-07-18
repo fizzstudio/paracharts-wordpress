5a2522c16699c48087b08ed2ad340b7ddcff3648
Joe Dolson <design@joedolson.com>
2025-07-18 10:26:23 -0500

    Esc post title & sanitize arguments before passing.

--- components/templates/iframe.php
+++ components/templates/iframe.php
@@ -7,13 +7,13 @@ $this->library( 'paracharts' );
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo get_the_title( $post->ID ); ?></title>
+        <title><?php echo esc_html( get_the_title( $post->ID ) ); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_print_scripts( apply_filters( 'paracharts_iframe_scripts', $scripts, $post->ID ) ); ?>
     </head>
 	<!-- overflow: hidden; prevents the iframe from scrolling -->
     <body style="overflow: hidden;">
-		<?php echo $this->get_chart( $post->ID, $_GET ); ?>
+		<?php echo $this->get_chart( $post->ID, map_deep( $_GET, 'sanitize_text_field' ) ); ?>
     </body>
 </html>
